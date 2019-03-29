2933f5fed75d012ac4e3f68ae71e628dde367db6
Chris Guikema <chris.guikema@dornerworks.com>
2019-03-29 09:21:08 -0400

    boot-args: Don't truncate DTB address
    This allows the DTB to be placed in high memory.
    
    Change-Id: I1f8e212d20ff3c077b941acf55b4952e0d88918b

--- components/VM/src/main.c
+++ components/VM/src/main.c
@@ -823,7 +823,7 @@ static int load_linux(vm_t *vm, const char *kernel_name, const char *dtb_name, c
     }
 
     /* Set boot arguments */
-    err = vcpu_set_bootargs(vm->vcpus[BOOT_VCPU], entry, MACH_TYPE, (uint32_t) dtb);
+    err = vcpu_set_bootargs(vm->vcpus[BOOT_VCPU], entry, MACH_TYPE, dtb);
     if (err) {
         printf("Error: Failed to set boot arguments\n");
         return -1;
